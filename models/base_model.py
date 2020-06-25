aa7a36419d019c0285ccba7567d0f0e11a03ce77
Taha Elleuch <Tahaeleuch@gmail.com>
2020-06-25 16:27:34 +0100

    accept diconnary as input

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,25 @@ description de la classe  de base
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ constructeur parametré"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if **kwargs:
+            for cle, valeur in kwargs.items():
+                if cle == "__class__":
+                    pass
+                elif (cle == "created_at") and isinstance(created_at, str):
+                    self.created_at = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    continue
+                elif (cle == "updated_at") and isinstance(updated_at, str):
+                    self.updated_at = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    continue
+                setattr(self, cle, valeur)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ fonction daffichage """
