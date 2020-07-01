da3d8acefac243030f7afd7aad9ce1c7a2bbae06
Taha Elleuch <Tahaeleuch@gmail.com>
2020-07-01 18:48:56 +0100

    console.py

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import models
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
 
 class HBNBCommand(cmd.Cmd):
     """Class to define consol's command"""
